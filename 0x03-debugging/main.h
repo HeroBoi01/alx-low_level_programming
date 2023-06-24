bd3c0537ace295ec998c077b4314666ec972f4aa
HeroBoi01 <baali.achraf01@gmail.com>
2023-06-23 18:31:26 -0700

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,1 +1,7 @@
-void positive_or_negative(int i);
+/**
+ * positive_or_negative - Prints if a number is positive, negative, or zero
+ * @i: The number to be checked
+ *
+ * Return: void
+ */
+void positive_or_negative(int i);void positive_or_negative(int i);
