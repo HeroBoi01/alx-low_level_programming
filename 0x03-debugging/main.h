52b5addf93f5d9981aeffa7e00de27195db4703b
HeroBoi01 <baali.achraf01@gmail.com>
2023-06-23 18:32:23 -0700

    Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 /**
  * positive_or_negative - Prints if a number is positive, negative, or zero
  * @i: The number to be checked
  *
  * Return: void
  */
-void positive_or_negative(int i);void positive_or_negative(int i);
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
