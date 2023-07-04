9a26eede6672f75a6c21119d37cd5de37771d456
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-03 18:02:08 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,17 +1,22 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-print_to_98(0);
-print_to_98(98);
-print_to_98(111);
-print_to_98(81);
-print_to_98(-10);
-return (0);
+void print_to_98(int n) {
+    int i;
+
+    if (n <= 98) {
+        for (i = n; i <= 98; i++) {
+            printf("%d", i);
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    } else {
+        for (i = n; i >= 98; i--) {
+            printf("%d", i);
+            if (i != 98) {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
 }
