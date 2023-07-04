14dd7932bb04bef2049da01280def08bdde63949
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-04 06:28:17 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,58 @@
 #include <stdio.h>
-#include <gmp.h>
+
+#define MAX_DIGITS 1000
 
 /**
- * print_fibonacci - Prints Fibonacci numbers up to the specified count
- * @count: The number of Fibonacci numbers to print
- */
-void print_fibonacci(int count)
-{
-    mpz_t first;          /* First Fibonacci number */
-    mpz_t second;         /* Second Fibonacci number */
-    mpz_t next;
-    int i;
-
-    mpz_init_set_ui(first, 1);
-    mpz_init_set_ui(second, 2);
-
-    gmp_printf("%Zd, %Zd", first, second); /* Print the first two Fibonacci numbers */
-
-    mpz_init(next);
-
-    for (i = 3; i <= count; i++)
-    {
-        mpz_add(next, first, second);
-        gmp_printf(", %Zd", next);
-        mpz_set(first, second);
-        mpz_set(second, next);
-    }
-
-    mpz_clear(first);
-    mpz_clear(second);
-    mpz_clear(next);
-
-    printf("\n");
-}
-
-/**
- * main - Entry point of the program
+ * main - Finds and prints the first 98 Fibonacci numbers.
  *
- * Return: Always 0 (success)
+ * Return: 0 on success
  */
 int main(void)
 {
-    print_fibonacci(100);
+    int i, j;
+    int fib1[MAX_DIGITS] = {0}, fib2[MAX_DIGITS] = {0}, fib_sum[MAX_DIGITS] = {0};
+    int carry = 0;
 
+    fib1[MAX_DIGITS - 1] = 1;
+    fib2[MAX_DIGITS - 1] = 2;
+
+    printf("%d, %d", fib1[MAX_DIGITS - 1], fib2[MAX_DIGITS - 1]);
+
+    for (i = 2; i < 98; i++)
+    {
+        for (j = MAX_DIGITS - 1; j >= 0; j--)
+        {
+            fib_sum[j] = fib1[j] + fib2[j] + carry;
+            carry = fib_sum[j] / 10;
+            fib_sum[j] %= 10;
+        }
+
+        printf(", ");
+
+        /* Print the calculated Fibonacci number */
+        for (j = 0; j < MAX_DIGITS; j++)
+        {
+            /* Skip leading zeros until the first non-zero digit is encountered */
+            if (fib_sum[j] != 0)
+            {
+                /* Print the remaining digits */
+                while (j < MAX_DIGITS)
+                {
+                    printf("%d", fib_sum[j]);
+                    j++;
+                }
+                break;
+            }
+        }
+
+        /* Update fib1 and fib2 */
+        for (j = 0; j < MAX_DIGITS; j++)
+        {
+            fib1[j] = fib2[j];
+            fib2[j] = fib_sum[j];
+        }
+    }
+
+    printf("\n"); /* Print a new line at the end */
     return 0;
 }
