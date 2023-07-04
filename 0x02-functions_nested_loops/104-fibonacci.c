0861200b4b8fc8f5bbf9b09875a563f8c237d006
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-04 04:13:30 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
 
-#define MAX_DIGITS 1000
+/**
+* print_fibonacci - Prints Fibonacci numbers up to 98 terms
+*/
+void print_fibonacci(void)
+{
+int n = 98;       /* Number of Fibonacci numbers to print */
+int first = 1;    /* First Fibonacci number */
+int second = 2;   /* Second Fibonacci number */
+int i;
+
+printf("%d, %d", first, second);  /* Print the first two Fibonacci numbers */
+
+for (i = 3; i <= n; i++)
+{
+int next = first + second;
+printf(", %d", next);
+first = second;
+second = next;
+}
+
+printf("\n");
+}
 
 /**
- * main - Finds and prints the first 98 Fibonacci numbers.
- *
- * Return: 0 on success
- */
+* main - Entry point of the program
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-int i, j;
-int fib1[MAX_DIGITS] = {0}, fib2[MAX_DIGITS] = {0}, fib_sum[MAX_DIGITS] = {0};
-int carry = 0;
-fib1[MAX_DIGITS - 1] = 1;
-fib2[MAX_DIGITS - 1] = 1;
-printf("%d, %d", fib1[MAX_DIGITS - 1], fib2[MAX_DIGITS - 1]);
-for (i = 2; i < 98; i++)
-{
-for (j = MAX_DIGITS - 1; j >= 0; j--)
-{
-fib_sum[j] = fib1[j] + fib2[j] + carry;
-carry = fib_sum[j] / 10;
-fib_sum[j] %= 10;
-}
-printf(", ");
-/* Print the calculated Fibonacci number */
-for (j = 0; j < MAX_DIGITS; j++)
-{
-/* Skip leading zeros until the first non-zero digit is encountered */
-if (fib_sum[j] != 0)
-{
-printf("%d", fib_sum[j]);
-break;
-}
-}
-/* Update fib1 and fib2 */
-for (j = 0; j < MAX_DIGITS; j++)
-{
-fib1[j] = fib2[j];
-fib2[j] = fib_sum[j];
-}
-}
-printf("\n"); /* Print a new line at the end */
+print_fibonacci();
+
 return (0);
 }
