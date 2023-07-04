535f8b8b3d6c12777e4ab3e8ab971212da53aeff
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-04 14:04:14 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,21 +7,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, j;
+int i, j;
 
-    /* Find the end of the destination string */
-    for (i = 0; dest[i] != '\0'; i++)
-        ;
+/* Find the end of the destination string */
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-    /* Concatenate the source string to the destination string */
-    for (j = 0; src[j] != '\0'; j++)
-    {
-        dest[i] = src[j];
-        i++;
-    }
+/* Concatenate the source string to the destination string */
+for (j = 0; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
 
-    /* Add the terminating null byte */
-    dest[i] = '\0';
+/* Add the terminating null byte */
+dest[i] = '\0';
 
-    return dest;
+return (dest);
 }
