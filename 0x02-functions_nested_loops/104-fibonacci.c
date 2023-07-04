c7ec5f387340086a9fc6044ebc8bede2ca955de2
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-04 04:54:32 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 
 /**
- * print_fibonacci - Prints Fibonacci numbers up to 98 terms
- */
+* print_fibonacci - Prints Fibonacci numbers up to 98 terms
+*/
 void print_fibonacci(void)
 {
-    unsigned long n = 98;       /* Number of Fibonacci numbers to print */
-    unsigned long first = 1;    /* First Fibonacci number */
-    unsigned long second = 2;   /* Second Fibonacci number */
-    unsigned long i;
+unsigned long n = 98;       /* Number of Fibonacci numbers to print */
+unsigned long first = 1;    /* First Fibonacci number */
+unsigned long second = 2;   /* Second Fibonacci number */
+unsigned long i;
 
-    printf("%lu, %lu", first, second);  /* Print the first two Fibonacci numbers */
+printf("%lu, %lu", first, second);  /* Print the first two Fibonacci numbers */
 
-    for (i = 3; i <= n; i++)
-    {
-        unsigned long next = first + second;
-        printf(", %lu", next);
-        first = second;
-        second = next;
-    }
+for (i = 3; i <= n; i++)
+{
+unsigned long next = first + second;
+printf(", %lu", next);
+first = second;
+second = next;
+}
 
-    printf("\n");
+printf("\n");
 }
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (success)
- */
+* main - Entry point of the program
+*
+* Return: Always 0 (success)
+*/
 int main(void)
 {
-    print_fibonacci();
+print_fibonacci();
 
-    return (0);
+return (0);
 }
