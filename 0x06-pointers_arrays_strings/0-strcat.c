756bc8b0f87425fe0544003a4e3ef2e383f24503
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-04 14:01:18 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Pointer to the destination string
+ * @src: Pointer to the source string
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    int i, j;
+
+    /* Find the end of the destination string */
+    for (i = 0; dest[i] != '\0'; i++)
+        ;
+
+    /* Concatenate the source string to the destination string */
+    for (j = 0; src[j] != '\0'; j++)
+    {
+        dest[i] = src[j];
+        i++;
+    }
+
+    /* Add the terminating null byte */
+    dest[i] = '\0';
+
+    return dest;
+}
