5a4773a705961dc57105c2ed90d582d9ffb653a4
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-03 20:11:00 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle using the '#' character
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+int row, column;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (row = 0; row < size; row++)
+{
+for (column = 0; column < size; column++)
+{
+if (column < size - row - 1)
+_putchar(' ');
+else
+_putchar('#');
+}
+_putchar('\n');
+}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_square(int size);
+void _putchar(char c);
+void print_triangle(int size);
 
 #endif /* MAIN_H */
