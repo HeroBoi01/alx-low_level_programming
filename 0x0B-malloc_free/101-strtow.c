4a43ed9616edc6c37f173eb4b7f39d6a1f1b5b5a
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-11 19:16:58 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 /**
 * count_words - Counts the number of words in a string
 * @str: The input string
@@ -29,6 +30,14 @@ int num_words, i, j, k, len;
 if (str == NULL || str[0] == '\0')
 return (NULL);
 num_words = count_words(str);
+if (num_words == 0)
+{
+words = malloc(1 * sizeof(char *));
+if (words == NULL)
+return (NULL);
+words[0] = NULL;
+return (words);
+}
 words = malloc((num_words + 1) * sizeof(char *));
 if (words == NULL)
 return (NULL);
@@ -48,9 +57,9 @@ free(words[k]);
 free(words);
 return (NULL);
 }
-for (k = 0; k < len; k++)
-words[i][k] = str[j++];
+strncpy(words[i], str + j, len);
 words[i][len] = '\0';
+j += len;
 }
 words[num_words] = NULL;
 return (words);
