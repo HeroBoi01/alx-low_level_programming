fb934d7d9cad481fdee101e7d2fdff293ac1ea25
HeroBoi01 <baali.achraf01@gmail.com>
2023-07-11 19:45:46 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -35,7 +35,7 @@ char **strtow(char *str)
 {
 char **words;
 int num_words, i, j, k, len;
-if (str == NULL || str[0] == '\0')
+if (str == NULL || str[0] == '\0' || str[0] == ' ')
 return (NULL);
 num_words = count_words(str);
 if (num_words == 0)
